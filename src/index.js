a1db4e1b8071865340232ad7a99a22d51378ecc0
RomanKovgan <R.kovgan88@gmail.com>
2022-12-07 15:57:23 +0300

    finished step four

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import 'bootstrap';
 import * as yup from 'yup';
 import onChange from 'on-change';
 import i18next from 'i18next';
-
+import axios from 'axios';
 
 
 
@@ -14,6 +14,12 @@ const render = (elements) => (path, value, prevValue) => {
   //  case 'form.valid':
   //    renderList();
   //    break;
+    case 'data.feeds':
+      renderFeeds(elements, state);
+      break;
+    case 'data.posts': 
+      renderPosts(elements,state);
+      break;
     case 'form.errors':
       renderErrors(elements, value, prevValue);
       break;
@@ -27,6 +33,8 @@ const elements = {
   inputUrl: document.querySelector('#url-input'),
   submitForm: document.querySelector('.btn'),
   feedbackUrl: document.querySelector('.feedback'),
+  feedsContainer: document.querySelector('.feeds'),
+  postsContainer: document.querySelector('.posts'),
 };
 
 const renderErrors = (elements, error, prevError) => {
@@ -52,6 +60,77 @@ const renderErrors = (elements, error, prevError) => {
   return;
   
 };
+
+const renderFeeds = (elements, state) => {
+  elements.feedsContainer.innerHTML = '';
+  const divCardBorder = document.createElement('div');
+  divCardBorder.classList.add('card', 'border-0');
+
+  const divCardBody = document.createElement('div');
+  divCardBody.classList.add('card-body');
+
+  const cardTitle = document.createElement('h2');
+  cardTitle.classList.add('card-title', 'h4');
+  cardTitle.textContent = 'Фиды';
+
+  divCardBody.append(cardTitle);
+  divCardBorder.append(divCardBody);
+
+  const listGroup = document.createElement('ul');
+  listGroup.classList.add('lsit-group', 'border-0', 'rounded-0');
+  console.log(state.data.feeds);
+  state.data.feeds.forEach((feed) => {
+    const itemFeed = document.createElement('li');
+    itemFeed.classList.add('list-group-item', 'border-0', 'border-end-0');
+    const feedTitle = document.createElement('h3');
+    feedTitle.classList.add('h6', 'm-0');
+    feedTitle.textContent = feed.title;
+    const feedDescription = document.createElement('p');
+    feedDescription.classList.add('m-0', 'small', 'text-black-50');
+    feedDescription.textContent = feed.description;
+    itemFeed.append(feedTitle, feedDescription);
+    listGroup.append(itemFeed);
+    console.log(itemFeed);
+  });
+  divCardBody.append(listGroup);
+  elements.feedsContainer.append(divCardBorder)
+
+}
+
+const renderPosts = (elements, state) => {
+  elements.postsContainer.innerHTML = '';
+  const divCardBorder = document.createElement('div');
+  divCardBorder.classList.add('card', 'border-0');
+
+  const divCardBody = document.createElement('div');
+  divCardBody.classList.add('card-body');
+
+  const cardTitle = document.createElement('h2');
+  cardTitle.classList.add('card-title', 'h4');
+  cardTitle.textContent = 'Посты';
+  divCardBody.append(cardTitle);
+
+  const listGroup = document.createElement('ul');
+  listGroup.classList.add('list-group', 'border-0', 'rounded-0');
+
+  state.data.posts.forEach((post) => {
+    const itemPost =  document.createElement('li');
+    itemPost.classList
+    .add('list-group-item', 'd-flex', 'justify-content-between', 'align-items-start', 'border-0', 'border-end-0');
+    
+    const postHref = document.createElement('a');
+    postHref.setAttribute('href', post.href);
+    postHref.textContent = post.title;
+    itemPost.append(postHref);
+    listGroup.append(itemPost);
+  });
+
+  divCardBody.append(listGroup);
+  divCardBorder.append(divCardBody);
+  const postsContainer = elements.postsContainer;
+  postsContainer.append(divCardBorder);
+}
+
 yup.setLocale({
   string: {
     url: ({ url }) => ({ key:'invalidUrl' }), 
@@ -80,6 +159,10 @@ i18n.init({
 const state = onChange({
   usedUrls: [],
   url: '',
+  data: {
+    feeds: [],
+    posts: []
+  },
   form: {
     valid: true,
     processState: 'filling',
@@ -105,10 +188,32 @@ elements.formRss.addEventListener('submit', (e) => {
   .catch((e) => {
     const err = e.errors.map((err) => err.key)
     state.form.errors = i18n.t(err);
-    console.log(state.form.errors);
     state.form.valid = false;
   });
-
+  axios({
+    method: 'get',
+    url: 'https://allorigins.hexlet.app/raw',
+    params: {
+      url: state.url, 
+      disableCash: true,
+    }
+  }) 
+    .then(response => {
+      const parser = new DOMParser();
+      const htmlString = parser.parseFromString(response.data,"application/xml");
+      const xml = htmlString.documentElement;
+      const fidTitle = htmlString.documentElement.querySelector('channel').querySelector('title');
+      const fidDescription = htmlString.querySelector('channel').querySelector('description');
+      const posts = htmlString.querySelectorAll('item');
+      const titlePosts = Array.from(posts).map((post) => {
+        const titlePost = post.querySelector('title').innerHTML;
+        const hrefPost = post.querySelector('link').innerHTML;
+        return {title: titlePost, href: hrefPost};
+      });
+      
+      state.data.feeds.unshift({ title: fidTitle.textContent, description: fidDescription.textContent });
+      state.data.posts.unshift(...titlePosts);
+    });
 });
 
 elements.inputUrl.addEventListener('input', (e) => {
